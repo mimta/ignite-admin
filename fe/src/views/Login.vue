adc26e5d7a25dc54a863013a6c7e73bf940d4244
mimta <mimta@users.noreply.github.com>
2018-08-03 21:05:16 +0100

    Update Login.vue

--- fe/src/views/Login.vue
+++ fe/src/views/Login.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="iadmin_login">
-    <h1 class="title">管理登录</h1>
+    <h1 class="title">Admin Login</h1>
     <el-form>
-      <el-form-item label="用户名">
+      <el-form-item label="Username">
         <el-input v-model="username"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="Password">
         <el-input v-model="password" @keyup.enter.native="onLogin" type="password"></el-input>
       </el-form-item>
     </el-form>
-    <el-button @click="onLogin" type="primary">登录</el-button>
+    <el-button @click="onLogin" type="primary">Login</el-button>
   </div>
 </template>
 
